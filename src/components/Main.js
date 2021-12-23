96fddb8590af3be6b0a45854cb197a0056f8cfa3
git init <butkepavan55@gmail.com>
2021-12-23 06:54:21 -0800

    socials added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Punklist from './Punklist'
+import './Main.css'
 
 function Main() {
     return (
@@ -8,16 +10,51 @@ function Main() {
                         <div className='punkContainer'>
 
                             <img className='selectedPunk'
-                         src={ activePunk.image_original_url}></img>
+                         src='https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu'></img>
                         </div>
                     
                     </div>
                         <div className='punkDetails' style={{color: '#f5f6fa'}}>
                                 <div className='title'>
-                                    {activePunk.name}
+                                    NoDeep Punk
                                     </div>
-                                <span className='itemNumber'></span>                      
+                                <span className='itemNumber'>.#3</span>                      
                     </div>
+
+                    <div  className='owner'>
+                        <div className='ownerImageContainer'>
+                            <img src='https://media-exp1.licdn.com/dms/image/C4E03AQExWPxut0K3pA/profile-displayphoto-shrink_200_200/0/1613138028550?e=1645660800&v=beta&t=cqKQmIzPuyFm4S9gkY38h4xI8oA-IpamL1ks-2YKvGk' ></img>
+                        </div>
+
+                        <div className='ownerDetails'></div>
+                        <div className='ownerNameAndHandle' >
+                            <div>0x7aa3b7500eB82405D5128D29b73C57423B97bB0a</div>
+                            <div className='ownerHandle'>@cleverprogrammer</div>
+                        </div>
+
+                        <div class="container">
+                                <a href="" class="icon">
+                                    <i class="facebook fab fa-facebook-f"></i>
+                                </a>
+                                <a href="" class="icon">
+                                    <i class="twitter fab fa-twitter"></i>
+                                </a>
+                                <a href="" class="icon">
+                                    <i class="instagram fab fa-instagram"></i>
+                                </a>
+                                <a href="" class="icon">
+                                    <i class="google fab fa-google"></i>
+                                </a>
+                                <a href="" class="icon">
+                                    <i class="linkedin fab fa-linkedin"></i>
+                                </a>
+                        </div>
+
+
+
+
+                    </div>  
+
 
                 </div >
 
