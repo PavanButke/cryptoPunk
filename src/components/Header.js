ad01c7f9aff5b4b2c51b6cbd4469b6a7668423cc
git init <butkepavan55@gmail.com>
2021-12-23 03:57:39 -0800

    axios-succed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,60 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+// import * as React from 'react';
+// import { styled, alpha } from '@mui/material/styles';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import InputBase from '@mui/material/InputBase';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import SearchIcon from '@mui/icons-material/Search';
 
 import './Header.css'
 import punkLogo from '../assets/header/cryptopunk-logo.png'
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.black, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.black, 0.25),
-    },
-    marginLeft: 10,
+import searchIcon from '../assets/header/search.png'
+import themeSwitchIcon from '../assets/header/theme-switch.png'
+// const Search = styled('div')(({ theme }) => ({
+//     position: 'relative',
+//     borderRadius: theme.shape.borderRadius,
+//     backgroundColor: alpha(theme.palette.common.black, 0.15),
+//     '&:hover': {
+//       backgroundColor: alpha(theme.palette.common.black, 0.25),
+//     },
+//     marginLeft: 10,
     
-    width: '50%%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width:'70%',
-    },
-  }));
+//     width: '50%%',
+//     [theme.breakpoints.up('sm')]: {
+//       marginLeft: theme.spacing(1),
+//       width:'70%',
+//     },
+//   }));
   
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
+//   const SearchIconWrapper = styled('div')(({ theme }) => ({
+//     padding: theme.spacing(0, 2),
+//     height: '100%',
+//     position: 'absolute',
+//     pointerEvents: 'none',
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }));
   
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
+//   const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//     color: 'inherit',
+//     '& .MuiInputBase-input': {
+//       padding: theme.spacing(1, 1, 1, 0),
+//       // vertical padding + font size from searchIcon
+//       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//       transition: theme.transitions.create('width'),
+//       width: '100%',
+//       [theme.breakpoints.up('sm')]: {
+//         width: '12ch',
+//         '&:focus': {
+//           width: '20ch',
+//         },
+//       },
+//     },
+//   }));
   
 
 const Header = () => {
@@ -67,7 +68,20 @@ const Header = () => {
             <div className='searchIconContainer'>
 
 
-            <Box sx={{ flexGrow: 1 }}>
+              <img src={searchIcon} />
+              
+              </div>
+
+              <input className='searchInput' placeholder='Find something may be a collection or a gift..'/>
+
+
+
+
+
+
+
+
+            {/* <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -98,14 +112,40 @@ const Header = () => {
           </Search>
         </Toolbar>
       </AppBar>
-    </Box>
+    </Box> */}
 
                 
-            </div>
+            
         <div/>
     
+
+
+
+    
+         </div>
+     
+     <div className='headerItems'>
+
+        <button className='btn12'>Drop</button>
+        <button className='btn12'>Marketplace</button>
+        <button className='btn12'>Create</button>
+
+     </div>
+    <div className='headerActions'>
+      <div className='themeSwitchContainer'>
+          <img src={themeSwitchIcon}></img>
+      </div>
+
     </div>
+
+    <div className='loginContainer'>
+
+      <button className='loginButton' >
+        Get in
+      </button>
+
     </div>
+</div>
     )
 }
 export default Header
