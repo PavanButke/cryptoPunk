1a8a03185fdbb2df5aba8f2a3460cb45be3c28bd
git init <butkepavan55@gmail.com>
2021-12-29 10:54:51 +0530

    current user tesed

--- src/App.js
+++ src/App.js
@@ -42,7 +42,8 @@ function App()  {
         <Route path="/Login/Signup">
           <AuthProvider>
             <Container className='d-flex  align-items-center justify-content-center' style={{minHeight:'100vh'}}>
-         <div className='w-100' style={{maxwidth:'400px'}}>  
+          
+           <div className='w-100' style={{maxwidth:'400px'}}>  
            <Signup/>
             </div>
         
